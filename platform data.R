736cf12aa6eb657c03d7befaede296cb43136be7
Zach <zach@forecastingresearch.org>
2023-05-23 20:25:09 -0400

    running on platform data doc as well

---
+++ platform data.R
@@ -0,0 +1,143 @@
+# This document is in-progress.
+
+#### Setup####
+rm(list = ls())
+# replace with own dir
+yourHome <- "~/Documents/FRI"
+library(googlesheets4)
+library(dplyr)
+library(qdap)
+library(data.table)
+gs4_auth()
+
+setwd(paste0(yourHome, "/voi-vod"))
+source("sources/functions.R")
+
+#### Import from P8 composite - Raw####
+#url <- "***REMOVED***/edit#gid=1396622057"
+url <- "***REMOVED***/edit#gid=0"
+
+# P8 composite sheet will be parsed as _decimals_ rather than 0-100 percentages.
+p8_composite <- read_sheet(url, "Raw")
+
+p8_composite <- p8_composite[,1:6]
+
+names(p8_composite) <- c("Name", "Camp", "PU", "C_id", "PC", "PUC")
+
+#### Get CID tags from P8 composite - Composite####
+
+cid_tags <- read_sheet(url, "Raw")
+cid_tags <- cid_tags %>% select(`C id`, Tag)
+
+# cid_tags <- read_sheet(url, "Composite [old]")
+# names(cid_tags) <- unlist(cid_tags[1, ])
+# cid_tags <- cid_tags %>% select(Id, Tag)
+# cid_tags$Id <- unlist(cid_tags$Id)
+# cid_tags <- cid_tags[2:nrow(cid_tags), ]
+
+#### Initialize composite sheet with Id and Tag####
+composite_sheet <- cid_tags
+composite_sheet <- unique(composite_sheet)
+
+#### Add columns for composite sheet, grouped by Concerned/Skeptical...
+# P(U),
+pu_newcols <- getProbComposite_p8("PU")
+composite_sheet <- bindComposite_p8(composite_sheet, "PU", pu_newcols)
+# P(C),
+pc_newcols <- getProbComposite_p8("PC")
+composite_sheet <- bindComposite_p8(composite_sheet, "PC", pc_newcols)
+
+# U-c correlation,
+cor_table <- p8_composite %>% select(C_id, PU, PC)
+cor_table <- cor_table[complete.cases(cor_table), ]
+
+C_ids <- unique(cor_table$C_id)
+
+PU_PC_correlation <- c()
+
+for (i in 1:length(C_ids)) {
+  cor_tbl_id <- cor_table %>% filter(C_id == C_ids[i])
+  correlate <- cor(cor_tbl_id$PU, cor_tbl_id$PC, method = "spearman")
+  PU_PC_correlation <- c(PU_PC_correlation, correlate)
+}
+
+composite_sheet <- composite_sheet %>% mutate(PU_PC_correlation = PU_PC_correlation)
+
+# P(U|C),
+puc_newcols <- getProbComposite_p8("PUC")
+composite_sheet <- bindComposite_p8(composite_sheet, "PUC", puc_newcols)
+
+# P(U|¬C),
+p8_composite <- p8_composite %>%
+  rowwise() %>%
+  mutate(punotc = punotc(pc = PC, puc = PUC, pu = PU))
+punotc_newcols <- getProbComposite_p8("punotc")
+composite_sheet <- bindComposite_p8(composite_sheet, "punotc", punotc_newcols)
+
+# Effect of C,
+# raw
+p8_composite <- p8_composite %>%
+  rowwise() %>%
+  mutate(effect_c_raw = PUC - punotc)
+# standardized_raw
+max_effects <- aggregate(abs(effect_c_raw) ~ Name, max, data = p8_composite)
+p8_composite <- p8_composite %>%
+  rowwise() %>%
+  mutate(max_effect = max_effects[max_effects$Name == Name, ]$"abs(effect_c_raw)")
+p8_composite <- p8_composite %>%
+  mutate(effect_c_standardized = effect_c_raw / max_effect)
+# absolute
+p8_composite <- p8_composite %>%
+  rowwise() %>%
+  mutate(effect_c_raw_abs = abs(effect_c_raw)) %>%
+  mutate(effect_c_standardized_abs = abs(effect_c_standardized))
+
+# raw
+effect_c_raw_abs_newcols <- getProbComposite_p8("effect_c_raw_abs")
+composite_sheet <- bindComposite_p8(composite_sheet, "effect_c_raw_abs_mean", effect_c_raw_abs_newcols)
+composite_sheet <- composite_sheet %>%
+  mutate(effect_c_raw_abs_mean_concerned_rank = rank(-effect_c_raw_abs_mean_concerned, ties.method = "min", na.last = "keep")) %>%
+  mutate(effect_c_raw_abs_mean_skeptical_rank = rank(-effect_c_raw_abs_mean_skeptical, ties.method = "min", na.last = "keep"))
+composite_sheet <- bindComposite_p8(composite_sheet, "effect_c_raw_abs_median", effect_c_raw_abs_newcols)
+composite_sheet <- composite_sheet %>%
+  mutate(effect_c_raw_abs_median_concerned_rank = rank(-effect_c_raw_abs_median_concerned, ties.method = "min", na.last = "keep")) %>%
+  mutate(effect_c_raw_abs_median_skeptical_rank = rank(-effect_c_raw_abs_median_skeptical, ties.method = "min", na.last = "keep"))
+# standardized
+effect_c_standardized_abs_newcols <- getProbComposite_p8("effect_c_standardized_abs")
+composite_sheet <- bindComposite_p8(composite_sheet, "effect_c_standardized_abs_mean", effect_c_standardized_abs_newcols)
+composite_sheet <- composite_sheet %>%
+  mutate(effect_c_standardized_abs_mean_concerned_rank = rank(-effect_c_standardized_abs_mean_concerned, ties.method = "min", na.last = "keep")) %>%
+  mutate(effect_c_standardized_abs_mean_skeptical_rank = rank(-effect_c_standardized_abs_mean_skeptical, ties.method = "min", na.last = "keep"))
+composite_sheet <- bindComposite_p8(composite_sheet, "effect_c_standardized_abs_median", effect_c_standardized_abs_newcols)
+composite_sheet <- composite_sheet %>%
+  mutate(effect_c_standardized_abs_median_concerned_rank = rank(-effect_c_standardized_abs_median_concerned, ties.method = "min", na.last = "keep")) %>%
+  mutate(effect_c_standardized_abs_median_skeptical_rank = rank(-effect_c_standardized_abs_median_skeptical, ties.method = "min", na.last = "keep"))
+
+# VoI (naive),
+p8_composite <- p8_composite %>%
+  rowwise() %>%
+  mutate(VoI_naive = VoI_naive(pu = PU, puc = PUC, pc = PC, punotc = punotc))
+VoI_naive_newcols <- getProbComposite_p8("VoI_naive")
+composite_sheet <- bindComposite_p8(composite_sheet, "VoI_naive", VoI_naive_newcols)
+
+# VoD (naive)
+
+#### Effect of C - plain rank and standardized rank####
+
+#### VoD naive - standardized, ranked####
+
+#### Reproduce "other quant analyses from doc in notes####
+
+#### bootstrapped standard errors####
+
+#### impute raw to completion####
+
+#### Effect of C - pct changes####
+
+#### write sheet####
+names(composite_sheet) <- gsub("VoI_naive", "VoI (naive)", names(composite_sheet))
+names(composite_sheet) <- gsub("punotc", "P(U|¬C)", names(composite_sheet))
+names(composite_sheet) <- gsub("PUC", "P(U|C)", names(composite_sheet))
+names(composite_sheet) <- gsub("PU", "P(U)", names(composite_sheet))
+names(composite_sheet) <- gsub("PC", "P(C)", names(composite_sheet))
+write_sheet(composite_sheet, url, sheet = "Composite")
